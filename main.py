cdf39e54e1ff3eae06dfcedd6228dd85ca67cc0d
Yoon Min <minusxix@gmail.com>
2024-03-26 12:59:35 +0700

    data

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 chrome_options = Options()
 
 import time
+import xlwings as xw
 
 path = r"C:\Users\kidfa\Desktop\Auto\chromedriver.exe" #123
 ser = Service(path)
@@ -15,40 +16,38 @@ browser.maximize_window()
 
 time.sleep(15)
 
-new = browser.find_element(By.XPATH, '//i[@class="fa fa-outline-add-new-contact-2 pre"]').click()
-
-time.sleep(2)
-
-#ko tìm thấy
-phone = browser.find_element(By.XPATH, '//input[@class="phone-i-input flx-1"]').send_keys('0766661910')
-
-time.sleep(2)
-
-search = browser.find_element(By.XPATH, '//div[@class="z--btn--v2 btn-primary large  --rounded"]').click()
-
-time.sleep(2)
-
-chat = browser.find_element(By.XPATH, '//div[@class="z--btn--v2 btn-secondary medium  --full-width"]').click()
-
-time.sleep(2)
-
-input = browser.find_element(By.XPATH, '//div[@id="input_line_0"]')
-input.click()
-input.send_keys('Testing project!')
-
-time.sleep(2)
-
-send = browser.find_element(By.XPATH, '//i[@class="fa fa-Sent-msg_24_Line pre"]').click()
-
-time.sleep(2)
-
-try:
-    add = browser.find_element(By.XPATH, '//div[@class="z--btn--v2 btn-neutral small  --rounded"]')
-    add.click()
+range = xw.Range('A1:A3').value
+for num in range:
+    if num is None or num.strip() == '':
+        continue
     time.sleep(2)
-    accept = browser.find_element(By.XPATH, '//div[@class="z--btn--v2 btn-primary large  --rounded"]')
-    accept.click()
-except NoSuchElementException:
-    print('Đã kết bạn!')
+    new = browser.find_element(By.XPATH, '//i[@class="fa fa-outline-add-new-contact-2 pre"]').click()
+    time.sleep(2)
+    try:
+        phone = browser.find_element(By.XPATH, '//input[@class="phone-i-input flx-1"]').send_keys(num)
+        time.sleep(2)
+        search = browser.find_element(By.XPATH, '//div[@class="z--btn--v2 btn-primary large  --rounded"]').click()
+        time.sleep(2)
+        chat = browser.find_element(By.XPATH, '//div[@class="z--btn--v2 btn-secondary medium  --full-width"]').click()
+        time.sleep(2)
+        input = browser.find_element(By.XPATH, '//div[@id="input_line_0"]')
+        input.click()
+        input.send_keys('Testing project!')
+        time.sleep(2)
+        send = browser.find_element(By.XPATH, '//i[@class="fa fa-Sent-msg_24_Line pre"]').click()
+        time.sleep(2)
+        try:
+            add = browser.find_element(By.XPATH, '//div[@class="z--btn--v2 btn-neutral small  --rounded"]')
+            add.click()
+            time.sleep(2)
+            accept = browser.find_element(By.XPATH, '//div[@class="z--btn--v2 btn-primary large  --rounded"]')
+            accept.click()
+        except NoSuchElementException:
+            print('Đã kết bạn!')
+
+    except NoSuchElementException:
+        print('Không tìm thấy!')
+        cancel = browser.find_element(By.XPATH, '//i[@class="fa fa-close f16 pre"]').click()
+        continue
 
 time.sleep(30)
